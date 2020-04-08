184b9ef20ad3fdb2555c78c7d4ba9d73df4a84ad
yoneyama-taiga <tai03.ga07@gmail.com>
2020-04-08 15:13:35 +0900

    [Add]新規投稿画面、toppage link

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,27 @@ class BooksController < ApplicationController
   end
 
   def index #投稿一覧表示
+    @books = Book.all
   end
 
   def show #詳細ページ
   end
 
   def new #新規投稿ページ（index内に記述）
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit #記事編集ページ
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
