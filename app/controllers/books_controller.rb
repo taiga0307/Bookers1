080df1bc22951b4c3c865d7e54af4e97c0b19fe1
yoneyama-taiga <tai03.ga07@gmail.com>
2020-04-08 20:53:14 +0900

    [Add]リンク先追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,10 +15,23 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(blog.id)
+    redirect_to book_path(book.id)
   end
 
   def edit #記事編集ページ
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
